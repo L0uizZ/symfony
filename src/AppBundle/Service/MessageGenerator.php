6b9356fbb2cf241cb6c493e07988a5f53ac5d8ef
Louis Hinz <louis.hinz@hotmail.de>
2017-05-18 16:25:54 +0200

    init commit

--- src/AppBundle/Service/MessageGenerator.php
+++ src/AppBundle/Service/MessageGenerator.php
@@ -5,6 +5,7 @@ class MessageGenerator
 {
     public function getHappyMessage()
     {
+
         $messages = [
             'You did it! You updated the system! Amazing!',
             'That was one of the coolest updates I\'ve seen all day!',
